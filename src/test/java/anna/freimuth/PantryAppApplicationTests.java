38579a8d6e8e769c53385df1cafa5c6f6a86e31c
Anna Freimuth <a.freimuth90@gmail.com>
2021-04-04 13:09:02 +0200

    first commit

---
+++ src/test/java/anna/freimuth/PantryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package anna.freimuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
